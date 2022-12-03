6b41fe24c186b021a5a81ac830522f09ccc17d98
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2022-12-03 12:52:24 +0530

    MAps

---
+++ LEARN/TemplatesBHRAMASTRA/STL/map.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main(){
+    
+    map<string,int> marks;
+    marks["Harry"]=98;
+    marks["Jack"]=59;
+    marks["Rohan"]=2;
+    map<string,int> :: iterator it=marks.begin();
+
+    for(it ; it!=marks.end() ; it++){
+        cout<<(*it).first<<" "<<(*it).second<<"\n";
+    }
+
+    return 0;
+}
--- LEARN/TemplatesBHRAMASTRA/STL/vectors.cpp
+++ LEARN/TemplatesBHRAMASTRA/STL/vectors.cpp
@@ -6,14 +6,30 @@ void display(vector<int> &v)
 {
     for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i] << " , ";
+        cout << v[i] << " ";
+        // cout<< v.at(i)<<endl;
+
     }
     cout << endl;
 }
 
 int main()
 {
-    
-
+    vector<int> vec1;
+    vector<int> vec2(3,12);
+    int element,size;
+//     cout<<"Enter the size of your vector"<<endl;
+//     cin>>size;
+//     for(int i=0 ; i<size ; i++){
+//         cout<<"Enter an element to add: ";
+//         cin>>element;
+//         vec1.push_back(element);
+//     }    
+//     display(vec1);
+//     vector<int> :: iterator iter = vec1.begin();
+// //-- initializing a itreator for with name iter and setting its position to the start ----//
+//     vec1.insert(iter,8008);//---- using the insert command of vector----//
+//     display(vec1);
+    display(vec2);
     return 0;
 }
